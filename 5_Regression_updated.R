cd6a396d844f95c956abfa720034b3d31bed14a2
Mei-Ling Feng <mefeng7@gmail.com>
2022-05-06 19:59:53 -0400

    New model set for selection (all species, habitat+time, PCs 1-2, Representative species from each PC trend, representative species from  one PC trend)

--- 5_Regression_updated.R
+++ 5_Regression_updated.R
@@ -8,27 +8,29 @@ library(performance)
 
 load("Objects and Data/4_dp_outs_towns_updated.rda")
 load("Objects and Data/0_species_list.rda")
+load("Objects and Data/5_DP_PCA_updated.rda")
 
 dp<-dp_outs_towns%>%
+  left_join(coord[,c("city","Year","week","Dim.1","Dim.2")], 
+            by = c("actual_city_town"="city","week"="week","year"="Year"))%>%
   #remove missing data
   filter(saidi!=0 & !(is.na(RWBL)))%>%
-  mutate(week=as.factor(week),
-         year=as.factor(year),
-         month=as.factor(month))
-
-#Don't use interactions
-#Do models with all species, representative species ( 1 from each of the 3 groups in PC1 and PC2), PC1 and PC2
-
-
-#Use R2 conditional (fixed + random; variation explained by random effect, adjusted for each town), 
-#
-#BIC weight (1 = small delta) as model ranking indices
-
-
-#xtable r package https://cran.r-project.org/web/packages/xtable/vignettes/xtableGallery.pdf 
-#xftbl <- xtableFtable(tbl, method = "compact")
-#                          print.xtableFtable(xftbl, booktabs = TRUE)
-
+    mutate(week=as.factor(week),
+           year=as.factor(year),
+          month=as.factor(month))%>%
+  dplyr::select(-c("Cultivated_Crops", "Shrub_Scrub","county.x","county.y","date","week"))%>%
+  #add indicators of spatio-temporal subsets of the data
+  #Forested areas, Developed areas, Summer season, Winter Season
+  mutate(Developed=ifelse(Developed>=quantile(Developed,0.75),1,0),
+         Forest=ifelse(Forest>=quantile(Forest,0.75),1,0),
+         Summer=ifelse(season=="summer",1,0),
+         Winter=ifelse(season=="winter",1,0))%>%
+  #Select representative species of each spatio-temporal trend
+  mutate(Migrant=RWBL,
+         Resident=RBWO,#Also no spatial trend group
+         Forest.sp=PIWO,
+         Urban.sp=EUST)#Also in the no temporal trend group
+  
 
 #-------------------------------------------------------------------------------------------
 #Check for correlation and collinearity between predictors
@@ -77,182 +79,191 @@ VIF(m2)
 
 
 
-#-------------------------------------------------------------------------------------------------
-#Model SAIDI with species DPs, habitat, and time
+#----------------------------------------------------------------------------------------------------------------------------------
+
+#Question 1: Does the inclusion of species activity improve SAIDI model performance over using only spatio-temporal trends?
+#############################################################################################################################
+
+#Create a set of models to compare performance
 
 
-#1. Determine if species activity explains additional variance than just time and habitat
+# 1. habitat and time
+lm.th<-lm(log_saidi~year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
 
-#Select a species*time*habitat model using backwards selection
-##############################################################
-#11 species (removing correlated blackbirds and woodpeckers) and their interactions with 12 months + forest habitat
+lmer.th<-lmer(log_saidi~year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
 
-  #Round 1.
-m.sth1<-lm(log_saidi~(TUVU*month)+(MODO*month)+(HOSP*month)+(OSPR*month)+(RTHA*month)+(RWBL*month)+
-  (PIWO*month)+(RBWO*month)+(NOFL*month)+(EUST*month)+(AMCR*month)+(TUVU*Forest)+(MODO*Forest)+(HOSP*Forest)+(OSPR*Forest)+
-  (RTHA*Forest)+(RWBL*Forest)+
-  (PIWO*Forest)+(RBWO*Forest)+
-  (NOFL*Forest)+(EUST*Forest)+(AMCR*Forest)+year+
-  +Barren_Land+Open_Water+Grassland,data=dp)
-
-m.sth1<-lm(log_saidi~(TUVU*season)+(MODO*season)+(HOSP*season)+(OSPR*season)+(RTHA*season)+(RWBL*season)+
-             (PIWO*season)+(RBWO*season)+(NOFL*season)+(EUST*season)+(AMCR*season)+(TUVU*Forest)+(MODO*Forest)+(HOSP*Forest)+(OSPR*Forest)+
-             (RTHA*Forest)+(RWBL*Forest)+
-             (PIWO*Forest)+(RBWO*Forest)+
-             (NOFL*Forest)+(EUST*Forest)+(AMCR*Forest)+year+
-             +Barren_Land+Open_Water+Grassland,data=dp)
-
-summary(m.sth1)
-#Significant predictors:
-#(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+(RWBL*Forest)+(RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+
-#(EUST*month)+(NOFL*month)+(PIWO*month)+(RWBL*month)+(RTHAmonth)+(NOFL*month)+(HOSP*month)+
-#Barren_Land+NOFL+RBWO+RWBL+RTHA+MODO+month3
-
-  #Round 2.
-m.sth2<-lm(log_saidi~(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+(RWBL*Forest)+
-             (RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+(EUST*month)+(NOFL*month)+
-             (PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-             Barren_Land,data=dp)
+summary(lm.th)
 
 
-m.sth2<-lm(log_saidi~(MODO*season)+(OSPR*season)+(RTHA*season)+(RWBL*season)+
-             (MODO*Forest)+(HOSP*Forest)+
-             (RTHA*Forest)+(RWBL*Forest)+
-             (PIWO*Forest)+EUST+RBWO+
-             (NOFL*Forest)+(EUST*Forest)+year+
-             +Barren_Land,data=dp)
+
+  # 2. all species, habitat, and time
+lm.sth<-lm(log_saidi~TUVU+MODO+HOSP+
+             OSPR+RTHA+RWBL+
+             PIWO+RBWO+NOFL+
+             EUST+AMCR+TUVU+
+             MODO+HOSP+OSPR+
+             RTHA+RWBL+PIWO+
+             RBWO+NOFL+EUST+AMCR+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+             data=dp)
+
+lmer.sth<-lmer(log_saidi~TUVU+MODO+HOSP+
+                 OSPR+RTHA+RWBL+
+                 PIWO+RBWO+NOFL+
+                 EUST+AMCR+TUVU+
+                 MODO+HOSP+OSPR+
+                 RTHA+RWBL+PIWO+
+                 RBWO+NOFL+EUST+AMCR+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+                 data=dp)
+
+summary(lm.sth)
 
 
-summary(m.sth2)
-#Significant predictors:
-#(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+(RWBL*Forest)+(RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+
-#(NOFL*month)+(PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-#Barren_Land+NOFL+RBWO+RWBL+RTHA+MODO+month5+month2+MODO+RTHA+RWBL+NOFL+Forest
+
+  # 3. species PCs, habitat, and time 
+lm.pcth<-lm(log_saidi~Dim.1+Dim.2+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
+
+lmer.pcth<-lmer(log_saidi~Dim.1+Dim.2+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
+
+summary(lm.pcth)
 
 
-#Round 3.
-m.sth3<-lm(log_saidi~(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+(RWBL*Forest)+
-             (RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+(NOFL*month)+
-             (PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-             Barren_Land,data=dp)
+#Question 2: Do diverse species activity patterns improve SAIDI model performance over using only single species patterns?
+#############################################################################################################################
 
-m.sth3<-lm(log_saidi~(MODO*season)+(RTHA*season)+(RWBL*season)+
-             (MODO*Forest)+(HOSP*Forest)+
-             (RTHA*Forest)+(RWBL*Forest)+
-             (PIWO*Forest)+EUST+RBWO+
-             (NOFL*Forest)+(EUST*Forest)+year+
-             +Barren_Land,data=dp)
-summary(m.sth3)
-#Significant predictors:
-#(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+(RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+
-#(NOFL*month)+(PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-#Barren_Land+NOFL+RBWO+RWBL+RTHA+MODO+month2+MODO+RTHA+RWBL+NOFL+Forest
+  # 4. representative species from each PC trend (resident, migrant, forest, and urban)
+lm.repth<-lm(log_saidi~Migrant+Resident+Forest.sp+Urban.sp+
+              year+month+
+              Barren_Land+Open_Water+Grassland+Forest,
+            data=dp)
 
+lmer.repth<-lmer(log_saidi~Migrant+Resident+Forest.sp+Urban.sp+
+                  year+month+
+                  Barren_Land+Open_Water+Grassland+Forest+
+                  (1|actual_city_town),
+                data=dp)
 
-#Round 4.
-m.sth4<-lm(log_saidi~(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+
-             (RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+(NOFL*month)+
-             (PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-             Barren_Land,data=dp)
-
-summary(m.sth4)
+summary(lm.repth)
 
 
-#Total Species removed: 4
+# 5. representative species from Urban PC trend
+lm.uth<-lm(log_saidi~Urban.sp+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
 
-#Use a mixed model to account for random effect of town
-m.sth4.lmer<-lmer(log_saidi~(EUST*Forest)+(NOFL*Forest)+(PIWO*Forest)+
-             (RTHA*Forest)+(HOSP*Forest)+(MODO*Forest)+(NOFL*month)+
-             (PIWO*month)+(RWBL*month)+(RTHA*month)+(NOFL*month)+(HOSP*month)+
-             Barren_Land+(1|actual_city_town),data=dp)
+lmer.uth<-lmer(log_saidi~Urban.sp+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
 
-m.sth3.lmer<-lmer(log_saidi~(MODO*season)+(RTHA*season)+(RWBL*season)+
-             (MODO*Forest)+(HOSP*Forest)+
-             (RTHA*Forest)+(RWBL*Forest)+
-             (PIWO*Forest)+EUST+RBWO+
-             (NOFL*Forest)+(EUST*Forest)+year+
-             Barren_Land+(1|actual_city_town),data=dp)
+summary(lm.uth)
 
-anova(m.sth3.lmer,m.sth3)
+
+# 6. representative species from Resident PC trend
+lm.resth<-lm(log_saidi~Resident+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
+
+lmer.resth<-lmer(log_saidi~Resident+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
+
+summary(lm.resth)
+
+
+# 7. representative species from Migrant PC trend
+lm.mth<-lm(log_saidi~Migrant+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
+
+lmer.mth<-lmer(log_saidi~Migrant+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
+
+summary(lm.mth)
+
+
+# 8. representative species from Forest PC trend
+lm.fth<-lm(log_saidi~Forest.sp+
+             year+month+
+             Barren_Land+Open_Water+Grassland+Forest,
+           data=dp)
+
+lmer.fth<-lmer(log_saidi~Forest.sp+
+                 year+month+
+                 Barren_Land+Open_Water+Grassland+Forest+
+                 (1|actual_city_town),
+               data=dp)
+
+summary(lm.fth)
+
+
+
+#Use a mixed model to account for random effect of town?
+
+anova(lmer.sth,lm.sth)
+anova(lmer.th,lm.th)
+anova(lmer.fth,lm.fth)
+anova(lmer.uth,lm.uth)
+anova(lmer.resth,lm.resth)
+anova(lmer.mth,lm.mth)
+anova(lmer.pcth,lm.pcth)
+anova(lmer.repth,lm.repth)
 #Random effect is significant
 
 
-#Select a Habitat and Time model using backwards selection
-#############################################################
-m.th1<-lm(log_saidi~month+year+Forest+Barren_Land+Open_Water+Grassland,data=dp)
-m.th1<-lm(log_saidi~season+year+Forest+Barren_Land+Open_Water+Grassland,data=dp)
-summary(m.th1)
-#Significant predictors: Barren_Land+Forest+month11+month7+month6 (Open_Water and Grassland get dropped just like the species*hab*time model subsets)
 
+#Compare model performance between models 
+models <- list(lmer.sth, lmer.th, lmer.fth,
+               lmer.uth, lmer.resth, lmer.mth,
+               lmer.pcth,lmer.repth)
 
-m.th2<-lm(log_saidi~month+year+Forest+Barren_Land,data=dp)
-m.th2<-lm(log_saidi~season+year+Forest+Barren_Land,data=dp)
-summary(m.th2)
-
-#Use a mixed model to account for random effect of town
-m.th2.lmer<-lmer(log_saidi~month+year+Forest+Barren_Land+(1|actual_city_town),data=dp)
-m.th22.lmer<-lmer(log_saidi~season+year+Forest+Barren_Land+(1|actual_city_town),data=dp)
-
-anova(m.th2.lmer,m.th2)
-#random effect is significant
-
-
-
-#Compare model performance between models with and without species in a table
-models <- list(m.sth3.lmer,m.sth4.lmer,m.th2.lmer,m.th22.lmer)
-
-names(models)<-c('Species.Time(season).Habitat','Species.Time(month).Habitat','Time(month).Habitat','Time(season).Habitat')
+names(models)<-c('All.Species_Time_Habitat','Time_Habitat','Forest.Species_Time_Habitat',
+                 'Urban.Species_Time_Habitat','Resident.Species_Time_Habitat','Migrant.Species_Time_Habitat',
+                 'Species.PC_Time_Habitat','All.PC.Trend.Species_Time_Habitat')
 
 
 var_compare<-compare_performance(models,rank=T)
 #add the number of coefficients in each model
 Fixed.Effects<-data.frame(Name=names(models),Fixed.Effects=unlist(lapply(models,function(x){length(fixef(x))})))
 var_compare<-left_join(var_compare,Fixed.Effects,by="Name")
+  #Focus on R squared's, BIC, and number of parameters for perfomance metrics
 
-write.csv(var_compare,"Outputs/predictor_selection_models_monthseason_updated.csv",row.names = F)
-
-
-
-#2.Compare models using species from distinct spatial and temporal patterns from the PCA
-#RBWO and RTHA represent residents vs OSPR and RWBL who represent summer migrants
-#PIWO and TUVU occupy rural areas vs HOSP and NOFL occupying urban areas
-m.migrant2<-lmer(log_saidi~(RWBL*season)+OSPR+year+
-                   Barren_Land+(1|actual_city_town),data=dp)
-m.resident2<-lmer(log_saidi~(RTHA*season)+RBWO+year+
-                    Barren_Land+(1|actual_city_town),data=dp)
-m.rural2<-lmer(log_saidi~TUVU+(PIWO*Forest)+year+
-                 Barren_Land+(1|actual_city_town),data=dp)
-m.urban2<-lmer(log_saidi~HOSP+(NOFL*season)+year+
-                 Barren_Land+(1|actual_city_town),data=dp)
+write.csv(var_compare,"Outputs/predictor_selection_models_updated.csv",row.names = F)
 
 
 
-#Compare model performance in a table
-#define list of models
-models2 <- list(m.sth2,m.resident2,m.migrant2,m.rural2, m.urban2)
 
-#specify model names
-names(models2) <- c('All.Species',
-                    'Residents', 
-                    'Migrants', 
-                    'Rural',
-                    'Urban')
-
-pattern_compare<-compare_performance(models2,rank=T)
-
-
-write.csv(pattern_compare,"Outputs/species_subset_models_updated.csv",row.names = F)
-
-#Multispecies model is the best
-#Bird outages seem most correlated with activity levels of spring/fall migrants
-#Time over Habitat
-
-sth.res = resid(m.sth2)
-hist(sth.res)
+#Question 3: Does the inclusion of species activity improve SAIDI model performance over using only spatio-temporal trends?
+#############################################################################################################################
 
 #Fit 1 model, 
 #make indicator of observations above 75th quantile for habitat
-#mkae indicator of observation just within summer and winter
+#make indicator of observation just within summer and winter
 #use interactions with each indicator
 
 #summary table is coeff for each species, and then the interaction for each indicator (add species coeff to interaction coeff)
